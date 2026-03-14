bd6fc1fcf53ec2b34d3198e59897984575aa8088
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-03-14 23:38:45 +0530

    Advance useState and Batch update conept

--- 10_useState_Advance/src/App.jsx
+++ 10_useState_Advance/src/App.jsx
@@ -1,18 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useSyncExternalStore } from 'react'
 
 const App = () => {
-  const [num, setNum] = useState({name:"Aryan", age: 18});
-
-  let numClicked = ()=>{
+      //    const [num, setNum] = useState({name:"Aryan", age: 18});
     
-     setNum({name:"Sarthak", age: 15})
-  }
+      //      const btnClicked = ()=>{
+      //     setNum(prev => ({...prev, age: 20}));
+         
+      //  }
+
+      // Batch Update concept
+   const [num, setNum] = useState(10);
+
+   const btnClicked = () => {
+          setNum(prev => (prev + 1))
+          setNum(prev => (prev + 1))
+          setNum(prev => (prev + 1))
+          setNum(prev => (prev + 1))
+   }
 
   return (
+    <>
     <div>
-      <h1>{num.name} : {num.age}</h1>
-      <button onClick={numClicked} className='h-10 w-20 bg-red-900 '>Click</button>
+      <h1>{num}</h1>
+      <button onClick={btnClicked} className='h-10 w-20 bg-red-900 '>Click</button>
     </div>
+
+    </>
   )
 }
 
