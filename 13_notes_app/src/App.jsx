b38e96ebae49fbec4c93e3d93e8e5d9c97fe535a
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-03-15 13:50:43 +0530

    notes app

---
+++ 13_notes_app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form >
+        <input type="text" placeholder='Enter Notes Heading'/>
+        <textarea name="" id="" placeholder='Enter Deatils'></textarea>
+      </form>
+    </div>
+  )
+}
+
+export default App
