bfe9e2b1456a879a18d141d4f0df8542c8819eb0
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2026-03-21 12:42:28 +0530

    routing advanced

---
+++ 19_routing_advanced/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div className='bg-cyan-900 h-15 flex items-center justify-between w-full'>
+      <h2 className='text-2xl font-bold text-white pl-5'>Aryan</h2>
+      <div className='flex text-white  gap-10 pr-7 text-lg'>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 19_routing_advanced/src/pages/Notfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div>
+      <h3 className='flex justify-center mt-50 font-bold text-4xl text-red-500'>404 ERROR: Page Not Found</h3>
+    </div>
+  )
+}
+
+export default Notfound
